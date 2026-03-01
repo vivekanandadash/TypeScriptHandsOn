9ea0b83ffc895812776bc71baf866470e72b404e
Vivekananda <vivekanandadash245@gmail.com>
2026-03-02 00:28:04 +0530

    add string and boolean usecases and conversion

--- 04_StringAndBoolean/string.js
+++ 04_StringAndBoolean/string.js
@@ -1,1 +1,18 @@
+// how many types we can define string 
 var str = "vivekananda Dash";
+var str1 = 'vivekananda Dash gudulu';
+var str2 = 'vivekananda Dash madana';
+// converting from number to string 
+var num = 24;
+var info = "my name is ".concat(str, " and my age is ").concat(num);
+console.log(info);
+var data = num.toString();
+var data1 = "" + num;
+// Converting from boolean to string
+var boolData = true;
+var conv = boolData.toString();
+//use case of boolean
+var item = true;
+var item1;
+item1 = true;
+console.log(item1);
--- 04_StringAndBoolean/string.ts
+++ 04_StringAndBoolean/string.ts
@@ -1,1 +1,27 @@
-var str:string="vivekananda Dash"
+// how many types we can define string 
+var str:string="vivekananda Dash";
+var str1:string='vivekananda Dash gudulu';
+var str2:string='vivekananda Dash madana';
+ 
+// converting from number to string 
+
+var num:number=24;
+var info =`my name is ${str} and my age is ${num}`;
+console.log(info);
+
+var data:string=num.toString();
+ var data1:string=""+num;
+
+// Converting from boolean to string
+ var boolData=true;
+ var conv:string=boolData.toString();
+
+ //use case of boolean
+ var item:boolean=true;
+ var item1:boolean;
+ item1=true;
+console.log(item1);
+
+
+
+ 
