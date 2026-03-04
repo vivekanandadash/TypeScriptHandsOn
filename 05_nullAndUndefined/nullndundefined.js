cb242c691f04ee5e1a2de13d2759794c14457819
Vivekananda <vivekanandadash245@gmail.com>
2026-03-04 19:54:38 +0530

    learn about null and undefined varriable

---
+++ 05_nullAndUndefined/nullndundefined.js
@@ -0,0 +1,17 @@
+var tiger = undefined;
+var lion = null;
+if (tiger === lion) {
+    console.log("liger");
+}
+else {
+    console.log("liger nai");
+}
+var age = null;
+var userName = null;
+var login = true;
+console.log(typeof age);
+if (login) {
+    userName = "Vivekananda Dash";
+    age = 24;
+    console.log("user Name is ".concat(userName, " and user age is ").concat(age));
+}
--- 05_nullAndUndefined/nullndundefined.ts
+++ 05_nullAndUndefined/nullndundefined.ts
@@ -1,1 +1,19 @@
-var tiger=null;
+var tiger:undefined | string=undefined;
+var lion:null=null;
+if(tiger===lion){
+    console.log("liger"); 
+}else{
+    console.log("liger nai");
+    
+}
+
+var age : null|number=null;
+var userName:null|string=null;
+var login:boolean = true;
+console.log(typeof age);
+if(login){
+    userName="Vivekananda Dash";
+    age=24;
+    console.log(`user Name is ${userName} and user age is ${age}`);
+}
+
