3304ae62434056d6cb2085a2f46a463b711aa358
Vivekananda <vivekanandadash245@gmail.com>
2026-03-04 22:50:55 +0530

    add config file

---
+++ 06_configuration/config.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tiger = "animal";
+console.log(tiger);
+//# sourceMappingURL=config.js.map
---
+++ 06_configuration/config.ts
@@ -0,0 +1,6 @@
+var tiger:string="animal";
+console.log(tiger);
+
+// how to configure a tsc file
+//step 1 -> use - tsc --init
+//step-2 -> use - tsc ---- to generate essential config  file now you no need to genrate ,js file manually
