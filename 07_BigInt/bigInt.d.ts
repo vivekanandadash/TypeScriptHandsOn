32b38167096c267935ea2b43dfb843450e671ddd
Vivekananda <vivekanandadash245@gmail.com>
2026-03-11 00:06:25 +0530

    add big int example and interview questions and answer

---
+++ 07_BigInt/bigInt.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=bigInt.d.ts.map
--- 07_BigInt/bigInt.js
+++ 07_BigInt/bigInt.js
@@ -1,12 +1,12 @@
+"use strict";
 // in this topic we discuss about what is bigint
 //when we should use it
 //Example
 //Mixing Issues //when we use big in we need to add n in that and we cant add number with bigint so for th number also we add here n
 //Interview Questions //how to cover a number to bigint->using n int he last , mixing issues and what is the limit of a number 2 to the power 53-1
-
-
-var bigNumber:bigint = 9007199254740991n;
+Object.defineProperty(exports, "__esModule", { value: true });
+var bigNumber = 9007199254740991n;
 var oneSmall = 1n;
-var oneAnotherSamll=7n;
-console.log(bigNumber+oneSmall+oneAnotherSamll);
-
+var oneAnotherSamll = 7n;
+console.log(bigNumber + oneSmall + oneAnotherSamll);
+//# sourceMappingURL=bigInt.js.map
